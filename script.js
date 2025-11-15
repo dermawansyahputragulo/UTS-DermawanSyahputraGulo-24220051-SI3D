c8427c03c8c4c0d1c9094579793d74c93ba23893
Dermawan Syahputra Gulo <mawangulo133@gmail.com>
2025-11-15 16:08:26 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 
+let satukaliAlert = false;
+
+document.getElementById("playbacksound").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (!satukaliAlert) {
+    alert("Klik 1x untuk play backsound, \ndan klik 1x lagi untuk stop backsound:D");
+    satukaliAlert = true;
+  }
+});
+
+const audio = new Audio("Assets/backsound.ogg");
+audio.preload = "auto";
+let isPlaying = false;
+
+audio.load();
+
+function onbacksound() { 
+  document.getElementById("playbacksound").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!isPlaying) {
+        audio.play();
+        isPlaying = true;
+    } else {
+        audio.pause();
+        audio.currentTime = 0; 
+        isPlaying = false;
+        location.reload();
+    }
+});
+}
+
+document.getElementById("reload").addEventListener("click", function () {
+  location.reload();
+});
+
+document.getElementById("exit").addEventListener("click", function () {
+  window.close(); // coba tutup tab
+});
+
 function home(){
     document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas SI-3X";
-    document.getElementById('aktifi').style.backgroundColor="white";
-    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('progres-kelas-content').style.backgroundColor="white";
+    document.getElementById('aktifitas-selanjutnya-content').style.backgroundColor="white";
     document.getElementById('aside').style.backgroundColor="white";
-    document.getElementById('lmpr').style.backgroundColor="white";
+    document.getElementById('lampiran-content').style.backgroundColor="white";
 }
 
 function mahasiswa () {
@@ -16,32 +56,32 @@ function kelas() {
 }
 
 function aktifitas(){
-    document.getElementById('aktifi').style.backgroundColor="#1d00fcff";
-    document.getElementById('aktifi1').style.backgroundColor="#4400ffff";
+    document.getElementById('progres-kelas-content').style.backgroundColor="grey";
+    document.getElementById('aktifitas-selanjutnya-content').style.backgroundColor="grey";
     document.getElementById('aside').style.backgroundColor="white";
-    document.getElementById('lmpr').style.backgroundColor="white";
+    document.getElementById('lampiran-content').style.backgroundColor="white";
 }
 
 function statistik(){
-    document.getElementById('aside').style.backgroundColor="#e35b5bff";
-    document.getElementById('lmpr').style.backgroundColor="white";
-    document.getElementById('aktifi').style.backgroundColor="white";
-    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="grey";
+    document.getElementById('lampiran-content').style.backgroundColor="white";
+    document.getElementById('progres-kelas-content').style.backgroundColor="white";
+    document.getElementById('aktifitas-selanjutnya-content').style.backgroundColor="white";
     
 }
 
 function dokumen(){
-    document.getElementById('lmpr').style.backgroundColor="#000153ff";
+    document.getElementById('lampiran-content').style.backgroundColor="grey";
     backgroundColor="white";
-    document.getElementById('aktifi').style.backgroundColor="white";
-    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('progres-kelas-content').style.backgroundColor="white";
+    document.getElementById('aktifitas-selanjutnya-content').style.backgroundColor="white";
     document.getElementById('aside').style.backgroundColor="white";
 }
 
 function singout(){
     document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas";
-    document.getElementById('aktifi').style.backgroundColor="white";
-    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('progres-kelas-content').style.backgroundColor="white";
+    document.getElementById('aktifitas-selanjutnya-content').style.backgroundColor="white";
     document.getElementById('aside').style.backgroundColor="white";
-    document.getElementById('lmpr').style.backgroundColor="white";
+    document.getElementById('lampiran-content').style.backgroundColor="white";
 }
